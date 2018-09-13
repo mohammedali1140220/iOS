0334b142a06c1c5f53665d8db384bd1e452609d5
mohammedali1140220 <mohammedali1140220@gmail.com>
2018-09-13 20:57:51 +0300

    Update PhotoViewController.swift

--- Task/View/PhotoViewController.swift
+++ Task/View/PhotoViewController.swift
@@ -5,21 +5,32 @@
 //  Created by Radix on 9/10/18.
 //  Copyright © 2018 Radix. All rights reserved.
 //
+// Name: PhotoViewController
+// Used to display the normal image 
 
+
+// define libraries 
 import UIKit
 import SDWebImage
 
+
+// define the class PhotoViewController
 class PhotoViewController: UIViewController {
 
-    
+    // define IBoutlet for image view to display the image 
     @IBOutlet weak var photoImageView: UIImageView!
+    
+    // define photo that recived from the pervious view controller 
     var photo = Photo()
     
+    // override view controller function
     override func viewDidLoad() {
+        
+        // call the super function
         super.viewDidLoad()
         
+        // set the image using sdWebImage 
         photoImageView.sd_setImage(with: URL(string: photo.url), placeholderImage: UIImage(named: "Landing.png"))
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {

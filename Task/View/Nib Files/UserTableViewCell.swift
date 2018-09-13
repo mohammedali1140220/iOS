53c8bb9dbb224da4d384d900c1b22050fd82621e
mohammedali1140220 <mohammedali1140220@gmail.com>
2018-09-13 21:06:24 +0300

    Update UserTableViewCell.swift

--- Task/View/Nib Files/UserTableViewCell.swift
+++ Task/View/Nib Files/UserTableViewCell.swift
@@ -5,12 +5,19 @@
 //  Created by Radix on 9/10/18.
 //  Copyright © 2018 Radix. All rights reserved.
 //
+// Name: UserTableViewCell
+// Used: Define cell for photos table view 
 
+// define libraries 
 import UIKit
 
+// define class UserTableViewCell as UITableViewCell
 class UserTableViewCell: UITableViewCell {
 
+    // define IBOutlet variable to binding data 
     @IBOutlet weak var userName: UILabel!
+    
+    // define IBOutlet variable to binding data 
     @IBOutlet weak var userEmail: UILabel!
     
     override func awakeFromNib() {
@@ -24,13 +31,24 @@ class UserTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+    /*
+    Name: handleCell
+    Parameters: cell: UserTableViewCell, indexPathRaw: Int, users: [Users]
+    Return: UITableViewCell
+    Used: handle each cell for each index path and return the cell 
+    **/
     func handleCell(cell: UserTableViewCell, indexPathRow: Int, users: [User]) -> UITableViewCell{
         
+        // define the user of current index path
         let user = users[indexPathRow]
         
+        // put the user name 
         cell.userName.text = user.name
+        
+        // put the user email 
         cell.userEmail.text = user.email
         
+        // return cell
         return cell
     }
     

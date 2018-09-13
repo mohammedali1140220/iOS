f0dbd772c18752906010b03eb96515213a38d995
mohammedali1140220 <mohammedali1140220@gmail.com>
2018-09-13 21:01:34 +0300

    Update PhotoTableViewCell.swift

--- Task/View/Nib Files/PhotoTableViewCell.swift
+++ Task/View/Nib Files/PhotoTableViewCell.swift
@@ -5,13 +5,22 @@
 //  Created by Radix on 9/10/18.
 //  Copyright © 2018 Radix. All rights reserved.
 //
+// Name: PhotoTableViewCell
+// Used to define the cell that will display photo
 
+
+// define libraries 
 import UIKit
 
+// define class PhotoTableViewCell as type of UITableViewCell
 class PhotoTableViewCell: UITableViewCell {
 
+    // define IBOutlet variable to binding data 
     @IBOutlet weak var title: UILabel!
+    
+    // define IBOutlet variable to binding data
     @IBOutlet weak var imageThumb: UIImageView!
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -24,13 +33,24 @@ class PhotoTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+    /*
+    Name : handleCell
+    Parameters : cell, IndexPathRaw, phtots
+    Return: UITableViewCell
+    Used: handle each cell before display it 
+    **/
     func handleCell(cell: PhotoTableViewCell, indexPathRow: Int, photos: [Photo]) -> UITableViewCell{
         
+        // define photo
         let photo = photos[indexPathRow]
         
+        // define cell title 
         cell.title.text = photo.title
+        
+        // define cell thumbnail using sdWebImage
         cell.imageThumb.sd_setImage(with: URL(string: photo.thumbnilURL), placeholderImage: UIImage(named: "Landing.png"))
         
+        // return cell
         return cell
     }
     
